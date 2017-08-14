a56894fa5c8a7c01e1f6c4bf035b9392be91d985
HannaVaitovich <30897096+HannaVaitovich@users.noreply.github.com>
2017-08-14 14:26:21 -0500

    Add files via upload

---
+++ lesson2.php
@@ -0,0 +1,32 @@
+<DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>PHP: Lesson 2</title>
+</head>
+<body>
+<?php
+$x = $x = rand(0,100);
+echo "Число ".$x;
+echo "<br/>";
+
+$var1 = 1;
+$var2 = 1;
+$var3 = 0;
+
+while ($var1 < $x) {
+$var3 = $var1;
+$var1 += $var2;
+$var2 = $var3;
+	}
+	if ($var1 > $x) {
+		echo "задуманное число НЕ входит в числовой ряд";
+	}
+	if ($var1 === $x) {
+		echo "задуманное число входит в числовой ряд";
+	}
+
+?>
+	
+</body>
+</html>
